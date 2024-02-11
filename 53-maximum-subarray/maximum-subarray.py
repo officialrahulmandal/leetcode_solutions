6bbac01a82457985bd54bacba68bb130c700fb26
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2024-02-11 08:12:20 +0530

    Time: 523 ms (82.82%) | Memory: 24.6 MB (82.05%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -7,21 +7,16 @@ class Solution(object):
         if len(nums)==1:
             return nums[0]
         len_arr=len(nums)
-        i=0
         j=0
         max_sum=-99999
         sum=0
         while(j<len_arr):
             
             sum+=nums[j]
-            #print(i,j,sum)
             max_sum=max(max_sum,sum)
             if sum<0:
-                j+=1
-                #j+=1
                 sum=0
-            else:
-                j+=1
+            j+=1
             
         return max_sum
         
