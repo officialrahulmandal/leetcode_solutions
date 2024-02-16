df5d0a88eb0d2db2601f8b1c08f933fde70782ae
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2024-02-16 06:49:03 +0530

    Time: 1137 ms (5.79%) | Memory: 27.9 MB (91.74%) - LeetSync

---
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.py
@@ -0,0 +1,40 @@
+import operator
+class Solution(object):
+    def findLeastNumOfUniqueInts(self, arr, k):
+        """
+        :type arr: List[int]
+        :type k: int
+        :rtype: int
+        """
+        store={}
+        #print(k)
+        for element in arr:
+            if element in store:
+                store[element]+=1
+            else:
+                store[element]=1
+        stored = list(store.values())
+        stored.sort()
+        #print(stored)
+        i=0
+        while(k>0):
+            if k==0:
+                break
+            if k>=stored[i]:
+                k-=stored[i]
+                stored.pop(0)
+                continue                    
+            else:
+                t=k
+                k-=stored[i]
+                stored[i]-=t
+                if stored[i]==0:
+                    stored.pop(0)
+                    continue 
+                
+                break
+        print(stored)
+                
+                
+        return len(stored)
+        
