687259781a56dcad818c4a8715555d911f88dea2
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2024-02-20 05:40:58 +0530

    Time: 104 ms (43.90%) | Memory: 12.7 MB (97.35%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,18 +1,11 @@
-class Solution(object):
+class Solution:
     def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
         nums.sort()
-        len_arr=len(nums)
-        i=0
-        while(i<len_arr):
-            if i!=nums[i]:
+        # print(nums)
+        for i in range(len(nums)):
+            if nums[i]!=i:
                 return i
-                break
-            i+=1
-        return i
+        return len(nums)
 
         
         
