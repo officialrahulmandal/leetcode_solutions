0f830388fc86f98d240dde9d02fdaaaa57f9de5f
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2024-02-22 22:11:08 +0530

    Time: 560 ms (48.57%) | Memory: 16.8 MB (65.36%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -5,6 +5,7 @@ class Solution:
         for a in trust:
             out_degree[a[0]] += 1
             in_degree[a[1]] += 1
+        print(in_degree,out_degree)
         for i in range(1, N + 1):
             if in_degree[i] == N - 1 and out_degree[i] == 0:
                 return i
