a5164a2897b103c96fac2e24e452e47ebf5b0887
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2024-02-28 21:47:06 +0530

    Time: 40 ms (7.79%) | Memory: 16.7 MB (46.10%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.py
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.py
@@ -1,16 +1,47 @@
-class Solution:
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def findBottomLeftValueHelper(self, root):
+        if root.left is None and root.right is None:
+            return 0, root.val
+        
+        left_d = None
+        right_d = None
+        left_v = 0
+        right_v = 0
+        if root.left is not None:
+            left_d, left_v = self.findBottomLeftValueHelper(root.left)
+        if root.right is not None:
+            right_d, right_v = self.findBottomLeftValueHelper(root.right)
+
+        if left_d is None:
+            value=right_v
+            diameter=right_d
+        elif right_d is None:
+            value = left_v
+            diameter=left_d
+        elif left_d<right_d:
+            value=right_v
+            diameter=right_d
+        else:
+            value = left_v
+            diameter=left_d
+
+        print(diameter, value)
+        return diameter+1, value
+
+        
+
+
     def findBottomLeftValue(self, root):
-        queue = deque([root])
-        leftmost_value = None
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        return self.findBottomLeftValueHelper(root)[1]
 
-        while queue:
-            node = queue.popleft()
-
-            leftmost_value = node.val
-
-            if node.right:
-                queue.append(node.right)
-            if node.left:
-                queue.append(node.left)
-
-        return leftmost_value
+        
