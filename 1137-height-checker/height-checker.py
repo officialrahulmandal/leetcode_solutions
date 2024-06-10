9c2d3323297a8e3e38ef060c571aad2e9faea381
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2024-06-10 20:48:22 +0530

    Time: 38 ms (64.91%) | Memory: 16.4 MB (77.64%) - LeetSync

---
+++ 1137-height-checker/height-checker.py
@@ -0,0 +1,13 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        freq=[0]*101
+        for x in heights:
+            freq[x]+=1
+        cnt=0
+        for x in range(1, 101):
+            f=freq[x]
+            for i in range(cnt, cnt+f):
+                heights[i]-=x
+            cnt+=f
+        return len(heights)-heights.count(0)
+        
