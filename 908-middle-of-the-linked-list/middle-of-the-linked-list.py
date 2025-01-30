277d9fc81bb92280ac08187c15eee62fa2b52e6b
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2025-01-30 18:52:45 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (59.72%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -1,10 +1,8 @@
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow_pointer = head
-        fast_pointer = head
-
-        while fast_pointer is not None and fast_pointer.next is not None:
-            slow_pointer = slow_pointer.next
-            fast_pointer = fast_pointer.next.next
-
-        return slow_pointer
+        fast=head
+        slow=head
+        while fast is not None and fast.next is not None:
+            fast=fast.next.next
+            slow=slow.next
+        return slow
