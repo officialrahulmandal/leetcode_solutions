47a29dd7e1448134dbef58f130a1041f7b300db4
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2025-02-01 06:01:47 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (39.56%) - LeetSync

--- 3429-special-array-i/special-array-i.py
+++ 3429-special-array-i/special-array-i.py
@@ -1,15 +1,23 @@
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
+        #variable to maintain last number
         last=0
+        #if only one value is present in array
         if len(nums)==1:
             return True
+        
+        #iteration to go through each element
         for n in nums:
+            #to be executed only once to set first value as last number
             if last==0:
                 last=n
+            # to check whether both last and current number is same(both odd or both even)
             elif last%2==n%2:
                 return False
+            #if not then continue further by updating current number as last number in last variable
             else:
                 last=n
+        # if we reach till last without any break that means it's an special array
         return True
 
 
