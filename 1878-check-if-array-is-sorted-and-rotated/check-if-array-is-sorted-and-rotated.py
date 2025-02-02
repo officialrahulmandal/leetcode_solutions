7ce0528784541c09fc36b447021ed29bcff7aaa2
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2025-02-02 23:11:15 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (47.79%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -1,44 +1,12 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        if len(nums)==0 or len(nums)==1:
-            return True
-        find_max=0
-        min_bef=9999
-        flip=False
-        last=None
-        for i in nums:
-            if last is None:
-                if find_max<i:
-                    find_max=i
-                if min_bef>i:
-                    min_bef=i
-                last=i
-                continue
-            elif flip and i>find_max:
-                return False
-            elif flip and i<find_max:
-                if min_bef<i:
-                    return False
-                if i<last:
-                    return False
-                else:
-                    last=i
-            else:
-                if i<last:
-                    if min_bef<i:
-                        return False
-                    if find_max<i:
-                        find_max=i
-                    flip=True
-                    last=i
-                else:
-                    if find_max<i:
-                        find_max=i
-                    if min_bef>i:
-                        min_bef=i
-                    last=i
-                    continue
-        return True
-
-
+        count = 0
+        n = len(nums)
         
+        for i in range(n):
+            if nums[i] > nums[(i + 1) % n]:
+                count += 1
+            if count > 1:
+                return False
+        
+        return True
