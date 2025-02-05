32fc35b7235a60f96b8ee8a885dafd54139b7b2d
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2025-02-05 16:16:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (47.67%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -1,57 +1,26 @@
-def partSort(arr, N, a, b):
-     
-    # Variables to store start and
-    # end of the index range 
-    l = min(a, b)
-    r = max(a, b)
- 
-    # Temporary array 
-    temp = [0 for i in range(r - l + 1)]
-    j = 0
-    for i in range(l, r + 1, 1):
-        temp[j] = arr[i]
-        j += 1
-     
-    # Sort the temporary array 
-    temp.sort(reverse = False)
- 
-    # Modifying original array with
-    # temporary array elements 
-    j = 0
-    for i in range(l, r + 1, 1):
-            arr[i] = temp[j]
-            j += 1
- 
-    # Print the modified array 
-    return arr
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i=j=len(nums)-1
+        while i>0 and nums[i-1]>=nums[i]:
+            i-=1
+            
+        if i==0:
+            nums.reverse()
+            return
+        
+        while nums[j]<=nums[i-1]:
+            j-=1
 
-class Solution(object):
-    def nextPermutation(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
-        len_arr=len(nums)
-        peak_index=-1
-        new_index=-1
-        for i in range(1,len_arr):
-            if nums[i]>nums[i-1]:
-                peak_index=i
-        if peak_index==-1:
-            for i in range(len_arr//2):
-                nums[i],nums[len_arr-i-1]=nums[len_arr-i-1],nums[i]
-        else:
-            for i in range(peak_index+1,len_arr):
-                if nums[i]<nums[peak_index] and nums[i]> nums[peak_index-1]:
-                    new_index=i
-            if new_index==-1:
-                nums[peak_index],nums[peak_index-1]=nums[peak_index-1],nums[peak_index]
-            else:
-                nums[new_index],nums[peak_index-1]=nums[peak_index-1],nums[new_index]
-            nums=partSort(nums,len(nums[peak_index:len_arr-1]),peak_index,len_arr-1)
+        nums[i-1],nums[j]=nums[j],nums[i-1]
+
+        nums[i:] = nums[len(nums)-1:i-1:-1]
+    
         return nums
-
-                    
+        
+            
 
 
         
