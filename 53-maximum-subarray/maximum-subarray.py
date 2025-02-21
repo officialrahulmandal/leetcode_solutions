cf20e2eecea203710ff0fd4fc3f5a6f18ab46e3c
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2025-02-21 18:50:32 +0530

    Time: 40 ms (86.84%) | Memory: 32.8 MB (24.32%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,22 +1,21 @@
-class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        i=0
-        len_arr=len(nums)
-        if len_arr==1:
-            return nums[0]
-        max_sum=float('-inf')
-        sum_i=float('-inf')
-        while (i<len_arr):
-            if sum_i<0:
-                sum_i=nums[i]
-            else:
-                sum_i+=nums[i]
-            max_sum=max(max_sum,sum_i)
-            i+=1
-        return max_sum
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        sp=0
+        curr=0
+        sum=0
+        final_sum=-99999
+        while curr<=(len(nums)-1):
+            sum=sum+nums[curr]
+            if sum>final_sum:
+                final_sum=sum
+            if sum<=0:
+                #sp=curr
+                sum=0
+            curr+=1
+        return final_sum
+                
+
             
+
+
         
