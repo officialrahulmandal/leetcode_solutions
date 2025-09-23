19fd964315098f81403fde756223d1962fb06d87
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2025-09-23 08:13:13 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (17.28%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.py
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -1,22 +1,16 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        def helper(s: str, idx: int) -> List[int]:
-            num = 0
-            while idx < len(s):
-                if s[idx] == '.':
-                    break
-                else:
-                    num = num * 10 + int(s[idx])
-                idx += 1
-            return [num, idx+1]
+        vs1, vs2 = version1.split("."), version2.split(".")
+        m, n = len(vs1), len(vs2)
 
-        i = j = 0
-        while(i < len(version1) or j < len(version2)):
-            v1, i = helper(version1, i)
-            v2, j = helper(version2, j)
+        for i in range(max(m, n)):
+            v1 = int(vs1[i]) if i < m else 0
+            v2 = int(vs2[i]) if i < n else 0
+
+            if v1 < v2:
+                return -1
+
             if v1 > v2:
                 return 1
-            elif v1 < v2:
-                return -1
 
         return 0
