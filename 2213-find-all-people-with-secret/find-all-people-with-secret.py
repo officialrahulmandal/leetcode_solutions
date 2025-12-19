69b2a2e69939642d42255869fa01952e7d021df3
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2025-12-19 08:41:32 +0530

    Time: 389 ms (34.40%) | Memory: 50.6 MB (87.90%) - LeetSync

--- 2213-find-all-people-with-secret/find-all-people-with-secret.py
+++ 2213-find-all-people-with-secret/find-all-people-with-secret.py
@@ -1,51 +1,34 @@
 class Solution:
-    def findAllPeople(self, n, meetings, firstPerson) :
-        # person 0, and firstPerson already knows the secret
-        knowsSecret = set([0, firstPerson])
-        
-        # after sort the meeting by time, the same time meeting will be grouped together
-        # so we can relie on people that knows secret before that time from "knowsSecret"
-        sortedMeetings = []
-        meetings.sort(key=lambda x:x[2]) # O(M*log(M))
-        
-        # tracking every meeting times to decide if current meeting has same time as previous meeting
-        seenTime = set()
-        
-        # building the sortedMeetings as explained above
-        for meeting in meetings: # O(M)
-            if meeting[2] not in seenTime:
-                seenTime.add(meeting[2])
-                sortedMeetings.append([])
-            sortedMeetings[-1].append((meeting[0], meeting[1]))
+    def findAllPeople(self, n, meetings, firstPerson):
+        parent = list(range(n))
 
-        # we then now will build graph for each meeting time, and traverse(BFS) the graph from
-        # people that already knows the secret to find more people who knows secret
-        for meetingGroup in sortedMeetings: #O(T)
-            # q needs to be set then push to queue, because of duplicate values we find from meetings
-            # otherwise it will reach the TLE
-            pplKnowSecret = set()
-            graph = defaultdict(list)
-            
-            # building bidirectional graph, and make sure to build our queue as well if we find
-            # a person who knows secret
-            for p1, p2 in meetingGroup: # ~O(M/T)
-                graph[p1].append(p2)
-                graph[p2].append(p1)
-                if p1 in knowsSecret:
-                    pplKnowSecret.add(p1)
-                if p2 in knowsSecret:
-                    pplKnowSecret.add(p2)
-                    
-            # adding people that already knows the secret to start off our graph traversal 
-            queue = deque((pplKnowSecret)) # O(n)
-        
-            while queue: # ~O(M/T)
-                curr = queue.popleft()
-                #any1 we find from our queue knows secret, so record it
-                knowsSecret.add(curr)
-                for neigh in graph[curr]:
-                    if neigh not in knowsSecret:
-                        knowsSecret.add(neigh)
-                        queue.append(neigh)
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
 
-        return list(knowsSecret)
+        def union(a, b):
+            parent[find(b)] = find(a)
+
+        meetings.sort(key=lambda x: x[2])
+
+        union(0, firstPerson)
+
+        i = 0
+        while i < len(meetings):
+            time = meetings[i][2]
+            involved = []
+
+            while i < len(meetings) and meetings[i][2] == time:
+                x, y, _ = meetings[i]
+                union(x, y)
+                involved.append(x)
+                involved.append(y)
+                i += 1
+
+            root0 = find(0)
+            for p in involved:
+                if find(p) != root0:
+                    parent[p] = p
+
+        return [i for i in range(n) if find(i) == find(0)]
