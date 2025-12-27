768117e439d6e7abfc337c398fd6429aca9fb2da
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2025-12-27 13:35:04 +0530

    Time: 183 ms (55.23%) | Memory: 51.1 MB (94.93%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.py
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.py
@@ -1,26 +1,22 @@
-import heapq
-
 class Solution:
-    def mostBooked(self, n: int, meetings: list[list[int]]) -> int:
-        meetings.sort(key=lambda x: x[0])
-        free = list(range(n))
-        heapq.heapify(free)
-        busy = []
-        cnt = [0]*n
-        for start, e in meetings:
-            while busy and busy[0][0] <= start:
-                _, r = heapq.heappop(busy)
-                heapq.heappush(free, r)
-            dur = e - start
-            if free:
-                r = heapq.heappop(free)
-                end = e
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        ready = [r for r in range(n)]
+        rooms = []
+        heapify(ready)
+        res = [0] * n
+
+        for s, e in sorted(meetings):
+            while rooms and rooms[0][0] <= s:
+                t, r = heappop(rooms)
+                heappush(ready, r)
+
+            if ready:
+                r = heappop(ready)
+                heappush(rooms, [e, r])
             else:
-                t, r = heapq.heappop(busy)
-                end = t + dur
-            cnt[r] += 1
-            heapq.heappush(busy, (end, r))
-        m = max(cnt)
-        for i, val in enumerate(cnt):
-            if val == m:
-                return i
+                t, r = heappop(rooms)
+                heappush(rooms, [t + e - s, r])
+
+            res[r] += 1
+
+        return res.index(max(res))
