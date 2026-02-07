16b34f47d21ff4de1ebc70203b4f9cd02dcf88d7
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2026-02-07 05:31:36 +0530

    Time: 151 ms (82.19%) | Memory: 20.1 MB (55.07%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
@@ -1,12 +1,10 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        n = len(s)
-        f = [0] * (n + 1)
-        b = 0
-        for i, c in enumerate(s, 1):
-            if c == 'b':
-                f[i] = f[i - 1]
-                b += 1
+        bCount = 0
+        minDeletions = 0
+        for char in s:
+            if char == 'a':
+                minDeletions = min(minDeletions + 1, bCount)
             else:
-                f[i] = min(f[i - 1] + 1, b)
-        return f[n]
+                bCount += 1   
+        return minDeletions
