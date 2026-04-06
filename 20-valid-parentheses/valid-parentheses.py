72a35f9b045d869df5ad2b94f4e7cb1a6cb1fe9c
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2026-04-06 11:41:03 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (44.61%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,34 +1,19 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        array=[]
-        for value in s:
-            if value=='(' or value=='{' or value=='[':
-                array.append(value)
-            elif len(array)==0:
-                return False
-            elif value=='}':
-                if array[-1]=='{':
-                    array.pop()
+class Solution:
+    def isValid(self, s: str) -> bool:
+        mapping={')':'(','}':'{',']':'['}
+        store=[]
+        for w in s:
+            if w not in mapping:
+                store.append(w)
+            else:
+                if len(store) !=0 and store[-1]==mapping[w]:
+                    store.pop()
+                    continue
                 else:
                     return False
-            elif value==')':
-                if array[-1]=='(':
-                    array.pop()
-                else:
-                    return False
-
-            elif value==']':
-                if array[-1]=='[':
-                    array.pop()
-                else:
-                    return False
-        if len(array)==0:
-            return True
-        else:
+        if len(store)!=0:
             return False
+        return True
+                
 
         
