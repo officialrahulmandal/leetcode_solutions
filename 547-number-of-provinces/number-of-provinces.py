e4624bffa804bb544c73cf13ef038a2d513d0dcb
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2026-05-16 23:26:20 +0530

    Time: 19 ms (9.40%) | Memory: 20.5 MB (53.37%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,44 +1,37 @@
 import queue
-
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        # building adj list
-
-        n = len(isConnected)
+        # first using bfs
+        v = len(isConnected)
         count = 0
-        adj = [[] for _ in range(n+1) ]
-        
-        for i in range(n):
-            for j in range(n):
+        adj = [[] for _ in range(v)]
+        visited = [False] * v
+        for i in range(v):
+            for j in range(v):
                 if i==j:
                     continue
-                if isConnected[i][j] == 1:
-                    adj[i+1].append(j+1)
+                if isConnected[i][j]==1:
+                    adj[i].append(j)
+                    #visited[i] =  True
+            
         
-        visited = set()
-
-        def bfs(node):
-            q = queue.deque()
-            q.append(node)
-            visited.add(node)
-
-            while q:
-                curr = q.popleft()
-                for i in adj[curr]:
-                    if i not in visited:
-                        q.append(i)
-                        visited.add(i)
-
-
-        for i in range(1, n+1):
-            if i not in visited:
-                bfs(i)
-                count +=1
-
-        return count
+        for i in range(v):
+            if not visited[i]:
+                q = queue.deque()
+                q.append(i)
+                visited[i] = True
+                count+=1
                 
+                while q:
+                    curr = q.popleft()
+                    visited[curr] = True
+                    for value in adj[curr]:
+                        if not visited[value]:
+                            q.append(value)
+                    
+        
+        return count
 
-            
-
-            
+        
+        
         
