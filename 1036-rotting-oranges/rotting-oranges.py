2fe88eb9128aeb98bbfd438c4c3ab8ada80a3424
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2026-05-17 11:08:16 +0530

    Time: 3 ms (76.36%) | Memory: 19.5 MB (12.54%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,30 +1,39 @@
-from collections import deque
+import queue
+
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        q=deque()
-        fresh,time=0,0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]==1:
-                    fresh+=1
-                elif grid[i][j]==2:
-                    q.append([i,j])
+        time =0
+        rows = len(grid)
+        columns = len(grid[0])
+        queues = queue.deque()
+        rotten = 0
+        good = 0
+        visited = set()
 
-        directions = [[0,1],[0,-1],[1,0],[-1,0]]
-        while q and fresh>0:
-            for i in range(len(q)):
-                c,r = q.popleft()
-                for dc,dr in directions:
-                    x,y=c+dc,r+dr
-                    if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and grid[x][y] == 1:
-                        grid[x][y] = 2
-                        q.append((x, y))
-                        fresh -= 1
-                    # grid[x][y]=2
-                    # q.append([x,y])
-                    # fresh-=1
+        for i in range(rows):
+            for j in range(columns):
+                if grid[i][j]==1:
+                    good+=1
+                elif grid[i][j]==2:
+                    rotten+=1
+                    queues.append((i,j))
+
+        while queues and good>0:
             time+=1
-        return time if fresh==0 else -1
+            for iter in range(len(queues)):
+                row,column = queues.popleft()
+                dim = [(0,1),(0,-1),(1,0),(-1,0)]
+                for r, c in dim:
+                    neigc = column+c
+                    neigr = row+r
+                    if 0<=neigc < columns and 0<=neigr<rows and grid[neigr][neigc] == 1:
+                        visited.add((row,column))
+                        grid[neigr][neigc]=2
+                        queues.append([neigr,neigc])
+                        good-=1
+                        rotten+=1
+        return -1 if good>0 else time
+
 
 
         
