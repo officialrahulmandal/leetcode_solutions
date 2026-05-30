f590f462c302e91388f06c960cf6cdcd90f42776
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2026-05-30 15:05:35 +0530

    Time: 2 ms (17.44%) | Memory: 19.8 MB (16.17%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,6 +1,6 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        visited =set()
+        if not image or not image[0]: return image
         row = len(image)
         column = len(image[0])
         if color == image[sr][sc]:
@@ -10,8 +10,7 @@ class Solution:
             direction = [[0,-1],[0,1],[1,0],[-1,0]]
 
             if sr>=0 and sr<=row-1 and sc>=0 and sc<=column-1:
-                if (sr,sc) not in visited and image[sr][sc] == oc:
-                    visited.add((sr,sc))
+                if image[sr][sc] == oc:
                     image[sr][sc]=color
                     for r, c in direction:
                         dfs(sr+r,sc+c, color, oc) 
