b2db3a9383559e87fd166be9d9ddb19dfa86791e
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2026-09-11 15:04:08 +0530

    Time: 3 ms (53.62%) | Memory: 20.5 MB (18.45%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,11 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        store = {}
-
+        covered = {}
         for i in range(len(nums)):
-            if store.get(target - nums[i],float(-inf)) != float(-inf):
-                return [store.get(target - nums[i]),i]
+            if target-nums[i] in covered:
+                return [covered[target-nums[i]], i]
             else:
-                store[nums[i]] = i
-            
-        return -1
+                covered[nums[i]]= i
